be66b37f3cf5f026c5cd8b7fb7424159700dc6c4
root309 <choco00101x@gmail.com>
2023-09-04 02:56:46 +0900

    Add Blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+// ブロックの種類
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+// ブロックの形状
+type BlockShape = [[usize; 4]; 4];
+const BLOCKS: [BlockShape; 7] = [
+    // Iブロック
+    [
+        [0,0,0,0],
+        [0,0,0,0],
+        [1,1,1,1],
+        [0,0,0,0],
+    ],
+    // Oブロック
+    [
+        [0,0,0,0],
+        [0,1,1,0],
+        [0,1,1,0],
+        [0,0,0,0],
+    ],
+    // Sブロック
+    [
+        [0,0,0,0],
+        [0,1,1,0],
+        [1,1,0,0],
+        [0,0,0,0],
+    ],
+    // Zブロック
+    [
+        [0,0,0,0],
+        [1,1,0,0],
+        [0,1,1,0],
+        [0,0,0,0],
+    ],
+    // Jブロック
+    [
+        [0,0,0,0],
+        [1,0,0,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+    // Lブロック
+    [
+        [0,0,0,0],
+        [0,0,1,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+    // Tブロック
+    [
+        [0,0,0,0],
+        [0,1,0,0],
+        [1,1,1,0],
+        [0,0,0,0],
+    ],
+];
 fn main() {
     let field = [
         [1,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -22,10 +86,23 @@ fn main() {
         [1,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
-
+    // 描画用フィールドの生成
+    let mut field_buf = field;
+    // 描画用フィールドにブロックの情報を書き込む
+    for y in 0..4 {
+        for x in 0..4 {
+            field_buf[y+ 2][x+2] = BLOCKS[BlockKind::I as usize][y][x];
+            field_buf[y+ 2][x+7] = BLOCKS[BlockKind::O as usize][y][x];
+            field_buf[y+ 6][x+2] = BLOCKS[BlockKind::S as usize][y][x];
+            field_buf[y+ 6][x+7] = BLOCKS[BlockKind::Z as usize][y][x];
+            field_buf[y+10][x+2] = BLOCKS[BlockKind::J as usize][y][x];
+            field_buf[y+10][x+7] = BLOCKS[BlockKind::L as usize][y][x];
+            field_buf[y+14][x+2] = BLOCKS[BlockKind::T as usize][y][x];
+        }
+    }
     for y in 0..21 {
         for x in 0..13 {
-            if field[y][x] == 1 {
+            if field_buf[y][x] == 1 {
                 print!("[]");
             }else {
                 print!(" .");
