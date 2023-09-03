a11fc247409ef213440620263ba875995a5ae859
root309 <choco00101x@gmail.com>
2023-09-04 03:22:37 +0900

    Update falling block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::{thread, time};
 // ブロックの種類
 enum BlockKind {
     I,
@@ -62,6 +63,11 @@ const BLOCKS: [BlockShape; 7] = [
         [0,0,0,0],
     ],
 ];
+struct Position {
+    x: usize,
+    y: usize,
+}
+
 fn main() {
     let field = [
         [1,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -86,28 +92,40 @@ fn main() {
         [1,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
-    // 描画用フィールドの生成
-    let mut field_buf = field;
-    // 描画用フィールドにブロックの情報を書き込む
-    for y in 0..4 {
-        for x in 0..4 {
-            field_buf[y+ 2][x+2] = BLOCKS[BlockKind::I as usize][y][x];
-            field_buf[y+ 2][x+7] = BLOCKS[BlockKind::O as usize][y][x];
-            field_buf[y+ 6][x+2] = BLOCKS[BlockKind::S as usize][y][x];
-            field_buf[y+ 6][x+7] = BLOCKS[BlockKind::Z as usize][y][x];
-            field_buf[y+10][x+2] = BLOCKS[BlockKind::J as usize][y][x];
-            field_buf[y+10][x+7] = BLOCKS[BlockKind::L as usize][y][x];
-            field_buf[y+14][x+2] = BLOCKS[BlockKind::T as usize][y][x];
-        }
-    }
-    for y in 0..21 {
-        for x in 0..13 {
-            if field_buf[y][x] == 1 {
-                print!("[]");
-            }else {
-                print!(" .");
+
+    let mut pos = Position { x: 4, y: 0 };
+
+    // 画面クリア
+    println!("\x1b[2J\x1b[H\x1b[?25l");
+
+    for _ in 0..5 {
+        // 描画用フィールドの生成（新しいfieldをコピーする）
+        let mut field_buf = field.clone();
+        // 描画用フィールドにブロックの情報を書き込む
+        for y in 0..4 {
+            for x in 0..4 {
+                if BLOCKS[BlockKind::I as usize][y][x] == 1 {
+                    field_buf[y+pos.y][x+pos.x] = 1;
+                }
             }
         }
-        println!();
+        // posのy座標を更新
+        pos.y += 1;
+        // フィールドを描画
+        println!("\x1b[H");  // カーソルを先頭に移動
+        for y in 0..21 {
+            for x in 0..13 {
+                if field_buf[y][x] == 1 {
+                    print!("[]");
+                } else {
+                    print!(" .");
+                }
+            }
+            println!();
+        }
+        // 1秒間スリープする
+        thread::sleep(time::Duration::from_millis(1000));
     }
+    // カーソルを再表示
+    println!("\x1b[?25h");
 }
